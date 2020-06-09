3a2e4ecfddc994a74192f28d24023d4d1c861e33
Loren Anderson <anderson.loren.james@gmail.com>
2020-06-09 15:31:31 -0500

    Added reward print statement to viewer

--- viewer.py
+++ viewer.py
@@ -1,15 +1,16 @@
-import gym
-import numpy as np
-
 import trial01
+
+import numpy as np
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
-PATH = "test_nets/test_0.000.dat"
+import gym
+
+PATH = "test_nets/test_239.758.dat"
 NUM_EPISODES = 10
-DEVICE = "cpu"
+device = "cpu"
 
 net = trial01.Net(8, 64, 4)
 net.load_state_dict(torch.load(PATH))
@@ -17,15 +18,18 @@ net.load_state_dict(torch.load(PATH))
 env = gym.make('LunarLander-v2')
 for _ in range(NUM_EPISODES):
     state = env.reset()
+    episode_reward = 0
     is_done = False
     while not is_done:
         env.render()
         state_a = np.array([state], copy=False)
-        state_v = torch.FloatTensor(state_a).to(DEVICE)
+        state_v = torch.FloatTensor(state_a).to(device)
         q_vals_v = net(state_v)
         _, act_v = torch.max(q_vals_v, dim=1)
         action = int(act_v.item())
         state, reward, is_done, _ = env.step(action)
+        episode_reward += reward
         if is_done:
             state = env.reset()
+            print("EPISODE REWARD: " + str(episode_reward))
 env.close()
